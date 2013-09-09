e246b5b417279dbb2f8dd360d52a436e7249eeb5
luccacy <luccacy@yahoo.com>
2013-09-09 13:31:03 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,65 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+'''
+Created on 2013-9-4
 
-version = '0.1.0'
+@author: zhouyu
+'''
 
-setup(name="knight",
-      version=version,
-      description='deploy openstack grizzly',
-      long_description="""
-A deploy package for openstack work on centos system
-""",
-      classifiers=["Development Status :: 1 - Production/Stable",
-                   "Intended Audience :: Developers",
-                   "License :: OSI Approved :: BSD License",
-                   "Programming Language :: Python",
-                   "Topic :: Internet :: WWW/HTTP",
-                   "Topic :: Software Development :: Libraries :: Python Modules",
-                   ],
-      author='zhouyu',
-      author_email='zhouyu@rockontrol.com',
-      url='http://rockontrol.com',
-      zip_safe=False,
-      install_requires=['eventlet','webob','iso8601','paste', 'pyparsing','netaddr','requests','pastedeploy', 'routes'],
-      packages=find_packages(exclude=['tests']),
+from distutils.core import setup
+import py2exe
+
+class Target:
+    def __init__(self, **kw):
+        self.__dict__.update(kw)
+        # for the versioninfo resources
+        self.version = "0.5.0"
+        self.company_name = "No Company"
+        self.copyright = "no copyright"
+        self.name = "knight"
+        
+manifest_template = '''
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<assemblyIdentity
+    version="5.0.0.0"
+    processorArchitecture="x86"
+    name="%(prog)s"
+    type="win32"
+/>
+<description>%(prog)s Program</description>
+<dependency>
+    <dependentAssembly>
+        <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="X86"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        />
+    </dependentAssembly>
+</dependency>
+</assembly>
+'''
+knightservice = Target(
+    # used for the versioninfo resource
+    description = "knight service",
+    # what to build.  For a service, the module name (not the
+    # filename) must be specified!
+    modules = ["knight.win_service"]
+    )
+
+includes = ['eventlet','webob','iso8601','knight', 'pyparsing','netaddr',
+            'requests','routes']
+
+options = {'py2exe':
+           {'compressed': 1,
+            'optimize': 2,
+            'includes' : includes        
+            }
+           }
+
+setup(
+      options = options,
+      zipfile = 'lib/share.zip',
+      service = [knightservice],      
       )
